b5183bf3a39fe21bab8acb6e7d9338f354460340
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-16 16:28:07 -0500

    Update final.py

--- final.py
+++ final.py
@@ -261,6 +261,8 @@ ax.annotate(f'mean_return: {mean_return:0.2f} %',
             )
 
 legend=ax.legend([f'mean_return(%)'])
+ax.tick_params(axis='y',which='both',left=True,right=True,direction='inout')
+ax.tick_params(axis='x',which='both',top=True,bottom=True,direction='inout')
 title_text=f'{user_ticker} | start_date: {start_date} | end_date: {end_date} | holding_days: {holding_time} | volume_threshold%: {volume_threshold} | price_change_threshold%: {pct_threshold}'
 plt.title(title_text)
 #plt.grid(True)
