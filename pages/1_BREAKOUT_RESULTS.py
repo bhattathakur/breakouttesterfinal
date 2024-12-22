37ec2124682b0c4adcfa10dff4adc95992045c34
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-21 21:51:29 -0500

    column name

--- pages/1_BREAKOUT_RESULTS.py
+++ pages/1_BREAKOUT_RESULTS.py
@@ -224,6 +224,7 @@ def color_val(val):
 #df_fin=df_final.style.applymap(color_val,subset=['return(%)'])#.format('{:.2f}')
  # Style the DataFrame
 #columns:
+st.write(f'column_name:',df_final.columns.name)
 st.write(df_final.columns.to_list())
 st.stop()
 df_final.drop(columns=[('Level1','Price')],inplace=True) #remove Price1 from the Level1
