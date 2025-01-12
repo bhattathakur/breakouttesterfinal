a71134c300121a27ba54675b432f0afbeea5fe34
Thakur Bhatta <bhattathakur2015@gmail.com>
2025-01-12 10:57:43 -0500

    changes

--- pages/3_BREAKOUT_SCREENER.py
+++ pages/3_BREAKOUT_SCREENER.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 #page config 
 st.set_page_config(page_title="Multi Parameter", page_icon=":smiley:", layout="wide", initial_sidebar_state="expanded")
+st.warning('COMING SOON...')
 
 
 
